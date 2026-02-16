67a573aa84987133e833ac18de568c1b1367862c
tundra_moss <156831370+AlexP-V@users.noreply.github.com>
2026-02-16 14:00:00 +0100

    camera behaviour fixed

--- GD6-Project2/Assets/Scripts/CameraController.cs
+++ GD6-Project2/Assets/Scripts/CameraController.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Camera cam;
+    // Camera starts zoomed out. This value is used at startup for initial zoom-out.
+    public float zoomOutSize = 7f;
+    // Tracks whether we've performed the initial zoom-in after the player's first click.
+    [HideInInspector]
+    public bool hasZoomedIn = false;
 
     void Awake()
     {
         if (cam == null) cam = GetComponent<Camera>();
         if (cam == null) cam = Camera.main;
+
+        // Ensure camera starts zoomed OUT before player interaction.
+        if (cam != null)
+        {
+            if (cam.orthographic)
+                cam.orthographicSize = zoomOutSize;
+            else
+                cam.fieldOfView = zoomOutSize;
+        }
     }
 
     // Centers camera on axial coordinate (flat-top) and optionally adjusts orthographic size to targetZoom.
     public IEnumerator CenterOnAxialCoroutine(int q, int r, float hexRadius, float duration, float targetZoom)
     {
+        // Mark that we've zoomed in (or at least triggered a zoom); used to prevent
+        // triggering the initial zoom multiple times.
+        hasZoomedIn = true;
+
         Vector2 world = HexGridUtility.AxialToWorld(q, r, hexRadius);
         Vector3 startPos = cam.transform.position;
         Vector3 targetPos = new Vector3(world.x, world.y, startPos.z);
